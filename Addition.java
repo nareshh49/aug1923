a3a73dc82e39d0b873e820214021a3a83718bdd2
nareshh49 <124552811+nareshh49@users.noreply.github.com>
2023-08-01 10:26:31 +0530

    New

---
+++ Addition.java
@@ -0,0 +1,9 @@
+ class Addition  
+    {  
+    public static void main(String args[])   
+    {  
+    int n1 = 1100, n2 = 400, sum;  
+    sum = n1 + n2;  
+    System.out.println("The sum of numbers is: "+sum);  
+    }  
+    }  
